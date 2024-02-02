4e9f1fc53aedf4a51a701ab80811d17e0b0b6e75
BillStephens2022 <two4onebill@yahoo.com>
2024-02-02 13:32:14 -0500

    initial commit - added Rest controller

---
+++ src/test/java/com/billstephens/springRestController/SpringRestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billstephens.springRestController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
